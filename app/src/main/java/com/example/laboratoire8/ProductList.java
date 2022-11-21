51eaec4189a69f79769b5d85f939ce6aed29756b
Ari0549 <91145371+Ari0549@users.noreply.github.com>
2022-11-21 17:28:07 -0500

    finished

---
+++ app/src/main/java/com/example/laboratoire8/ProductList.java
@@ -0,0 +1,39 @@
+package com.example.laboratoire8;
+
+/**
+ * Created by Miguel Garzón on 2017-05-09.
+ */
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ProductList extends ArrayAdapter<Product> {
+    private Activity context;
+    List<Product> products;
+
+    public ProductList(Activity context, List<Product> products) {
+        super(context, R.layout.activity_product_list, products);
+        this.context = context;
+        this.products = products;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View listViewItem = inflater.inflate(R.layout.activity_product_list, null, true);
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextView textViewName = (TextView) listViewItem.findViewById(R.id.textViewName);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextView textViewPrice = (TextView) listViewItem.findViewById(R.id.textViewPrice);
+
+        Product product = products.get(position);
+        textViewName.setText(product.getProductName());
+        textViewPrice.setText(String.valueOf(product.getPrice()));
+        return listViewItem;
+    }
+}
